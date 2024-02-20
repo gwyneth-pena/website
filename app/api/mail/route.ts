199f843509bb3329adb17c22c83bf8d9ff3ec51b
unknown <gwenpena21@gmail.com>
2024-02-20 11:25:59 +0800

    Fix return for sending email

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -3,12 +3,9 @@ import { NextRequest, NextResponse } from "next/server"
 
  
 export async function POST(request: NextRequest) {
-    try {
-        sendMail(await request.json()).then((response:any)=>{
-          console.log(response);
-        });
-        return NextResponse.json({ message: "Success!", status: 200 })
-      } catch (err) {
-        return NextResponse.json({ message: "Failed!", status: 500 })
-      }
+    return Promise.all([sendMail(await request.json())]).then(response=>{
+      return NextResponse.json({ message: "Success!", status: 200 })
+    }).catch(err=>{
+      return NextResponse.json({ message: "Failed!", status: 500 })
+    });
 }
