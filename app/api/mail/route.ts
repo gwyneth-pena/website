f866ee16f307b9097f2595a262897799675db94a
unknown <gwenpena21@gmail.com>
2024-02-20 10:45:14 +0800

    Added log for sent email

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -4,7 +4,9 @@ import { NextRequest, NextResponse } from "next/server"
  
 export async function POST(request: NextRequest) {
     try {
-        sendMail(await request.json());
+        sendMail(await request.json()).then((response:any)=>{
+          console.log(response);
+        });
         return NextResponse.json({ message: "Success!", status: 200 })
       } catch (err) {
         return NextResponse.json({ message: "Failed!", status: 500 })
